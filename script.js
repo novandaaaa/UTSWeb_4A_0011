eec5ec187e09c951155468130504ad46afbc5174
novandaaa <novanda.putra24@student.uisi.ac.id>
2026-05-28 23:40:52 +0700

    menambahkan styling untuk fungsi tombol dark mode dan lightmode, dan menambahkan logika js agar tulisan berubah saat ingin di click

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const darkBtn = document.getElementById("darkBtn");
 
 darkBtn.addEventListener("click", function(){
-
     document.body.classList.toggle("light-mode");
 
+    if(document.body.classList.contains("light-mode")){
+        darkBtn.textContent = "Dark";
+    } else {
+        darkBtn.textContent = "Light";
+    }
 });
