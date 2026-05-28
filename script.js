ea6984ead6164da11ad42119272b34c2d8cbbeba
novandaaa <novanda.putra24@student.uisi.ac.id>
2026-05-28 23:57:47 +0700

    memfungsikan tombol yang ada di section genre dan di popular, dan menambahan logika dibagian js

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ darkBtn.addEventListener("click", function(){
         darkBtn.textContent = "Light";
     }
 });
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const mangaItems = document.querySelectorAll(".manga-item");
+filterButtons.forEach(function(button){
+
+    button.addEventListener("click", function(){
+        const genre = button.dataset.genre;
+        mangaItems.forEach(function(item){
+
+            if(genre === "all"){
+
+                item.style.display = "block";
+            }
+            else if(item.dataset.genre === genre){
+
+                item.style.display = "block";
+            }
+            else{
+
+                item.style.display = "none";
+            }
+        });
+    });
+});
