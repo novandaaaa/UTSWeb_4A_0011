3010792b849092c18df24c259b850e7f5a3bc0b5
novandaaa <novanda.putra24@student.uisi.ac.id>
2026-05-29 00:11:03 +0700

    menambahkan logika untuk bagian contact us dan menambahkan class pada bagian form pada index section

--- script.js
+++ script.js
@@ -33,3 +33,21 @@ filterButtons.forEach(function(button){
         });
     });
 });
+
+const contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if(name === "" || email === "" || message === ""){
+        alert("Silahkan Isi Semua Kolom!");
+    }
+    else{
+
+        alert("Pesan Terkirim!");
+        contactForm.reset();
+    }
+});
